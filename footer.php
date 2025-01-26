234c0c1ecb51d183e594d9279efc1bb6715a49b4
danou85 <111521806+danou85@users.noreply.github.com>
2025-01-26 01:01:53 +0100

    modification ficher html

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- footer.php -->
+<footer>
+        <p>&copy; 2025 The Artbox. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Artbox</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <img src="images/logo.png" alt="Logo">
+        <h1>Bienvenue sur The Artbox</h1>
+    </header>
