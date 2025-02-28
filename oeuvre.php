c8f445b5b3ac803eeb23f6e2598f3e1b3ffc523b
danou85 <111521806+danou85@users.noreply.github.com>
2025-02-28 10:45:36 +0100

    supression des oeuvres

--- oeuvre.php
+++ oeuvre.php
@@ -1,98 +1,111 @@
-< ? php
-
-$oeuvre = [
+< ?php
+$oeuvres = [
     [
         "id" => 1,
         "titre" => "Dodomu",
         "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
+        "image" => "img/clark-van-der-beken.png",
+        "description" => "Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu (\"domicile\" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes."
+        ],
 
-    ],
-    [
+        [
         "id" => 2,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
+        "titre" => "Aashaaheen Baadal",
+        "artiste" => "Anaisha Devi",
+        "image" => "img/pawel-czerwinski-3.png",
+        "description" => "Sur cette oeuvre conceptuelle à la fois organique, minérale et liquide, Anaisha Devi nous transporte dans un nuage noir envoûtant. Un sombre tableau qui, par son verni éclatant, rayonne tel un marbre poli. Une oeuvre à la cohérence transcendantale, exécutée à la perfection."
     ],
     [
         "id" => 3,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
+        "titre" => "Nightlife Traffic",
+        "artiste" => "Andrew Forsythe",
+        "image" => "img/dan-cristian-padure.png",
+        "description" => "Quisque accumsan ultrices ligula vestibulum posuere. Aliquam feugiat ligula eget massa blandit condimentum. Morbi volutpat erat luctus suscipit pellentesque. Quisque cursus tempor nibh at sollicitudin. Sed blandit libero velit. Etiam tincidunt facilisis mollis."
     ],
     [
         "id" => 4,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
+        "titre" => "Le refuge de l'Havre",
+        "artiste" => "Simon Pelletier",
+        "image" => "img/steve-johnson-5.png",
+        "description" => "Nam tempus neque nec felis venenatis auctor. Nam velit risus, lobortis eu quam non, interdum efficitur nibh. Phasellus a augue ac orci lacinia mattis et vel lectus."
     ],
     [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
-    ],  [
-        "id" => 1,
-        "titre" => "Dodomu",
-        "artiste" => "Mia Tozerski",
-        "annee" =>Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
-
+        "id" => 5,
+        "titre" => "Red Washover",
+        "artiste" => "Kit Van Der Borght",
+        "image" => "img/steve-johnson.png",
+        "description" => "Nunc euismod ullamcorper tortor, id efficitur ante interdum in. Integer eu condimentum nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra."
     ],
+    [
+        "id" => 6,
+        "titre" => "Chromatics",
+        "artiste" => "Jean-Michel Delatronchette",
+        "image" => "img/pawel-czerwinski.png",
+        "description" => "Vivamus commodo non libero at hendrerit. In lacinia dui sit amet pellentesque iaculis. Donec at ultricies sem porttitor."
+    ],
+    [
+        "id" => 7,
+        "titre" => "Digital Negative",
+        "artiste" => "Hamish McKee",
+        "image" => "img/jazmin-quaynor.png",
+        "description" => "Integer in nisl posuere, pulvinar ex ac, tincidunt risus. Nullam vel lorem et leo dignissim accumsan."
+    ],
+    [
+        "id" => 8,
+        "titre" => "Blast from the past",
+        "artiste" => "Juliette Baskerville",
+        "image" => "img/steve-johnson-6.png",
+        "description" => "Nunc fermentum purus dapibus justo fermentum auctor. Maecenas non tincidunt leo. Morbi vitae iaculis sem."
+    ],
+    [
+        "id" => 9,
+        "titre" => "Hurricane",
+        "artiste" => "Natalie Wellington",
+        "image" => "img/victor-grabarczyk.png",
+        "description" => "Aliquam tristique tempus molestie. Nulla nisl eros, dapibus eu lectus in, cursus accumsan arcu."
+    ],
+    [
+        "id" => 10,
+        "titre" => "La marée rouge",
+        "artiste" => "Martin Rodriguez",
+        "image" => "img/pawel-czerwinski-2.png",
+        "description" => "Vivamus quis odio vel ligula feugiat facilisis. Donec eleifend pellentesque massa, ut malesuada est bibendum."
+    ],
+    [
+        "id" => 11,
+        "titre" => "Asimilacion",
+        "artiste" => "Angel Sanchez-Fernandez",
+        "image" => "img/steve-johnson-2.png",
+        "description" => "Mauris ut justo ac mi pretium eleifend. Curabitur sed magna ut elit facilisis pharetra."
+    ],
+
+    [
+        "id" => 12,
+        "titre" => "La Galaxia Gialla",
+        "artiste" => "Eduardo Tancredi",
+        "image" => "img/fly-d.png",
+        "description" => "Mauris maximus, orci sollicitudin ultrices elementum, tellus neque feugiat leo, quis lobortis purus neque vel lectus. Ut sagittis eros id lectus porttitor tincidunt. Donec scelerisque diam nec felis egestas, eget finibus ante porttitor. Sed malesuada sapien ut semper accumsan. Duis tristique dui vel egestas porttitor. Nunc tristique dapibus orci a amet."
+    ],
+    [
+        "id" => 13,
+        "titre" => "Puffy Amalgamate",
+        "artiste" => "Sandro De Blasi",
+        "image" => "img/orfeas-green.png",
+        "description" => "Donec semper a massa quis congue. In malesuada lorem ligula, ut posuere magna pulvinar in. Proin vitae enim gravida, commodo odio."
+    ],
+    [
+        "id" => 14,
+        "titre" => "Mirage",
+        "artiste" => "Stéphanie Kaiser",
+        "image" => "img/steve-johnson-4.png",
+        "description" => "Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam iaculis lorem ac ex tristique egestas et nec sapien. Donec tincidunt id erat sit amet tempus. Nullam vel molestie dui. Duis a neque massa. Vivamus quis ornare lacus. Nullam eleifend condimentum egestas. Vivamus magna purus, fermentum mollis mauris sed, consectetur interdum libero. Duis interdum tortor tellus, eget sollicitudin quis."
+    ],
+    [
+        "id" => 15,
+        "titre" => "Blaue Gelbe Muster",
+        "artiste" => "Adelheid Von Schreiber",
+        "image" => "img/steve-johnson-3.png",
+        "description" => "Curabitur dui odio, porta vel tempor sed, consectetur vitae mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus nec."
+    ]
+];
+?>
