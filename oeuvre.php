bcb5f33adac9e09e7000ab87f81d056f0ca9ed01
danou85 <111521806+danou85@users.noreply.github.com>
2025-03-03 12:22:57 +0100

    projet finit

--- oeuvre.php
+++ oeuvre.php
@@ -1,14 +1,39 @@
-<?php include('header.php');?>
+<?php
+require_once 'header.php';
+require_once 'oeuvres.php';
+
+// recuperer l'ID de l'oeuvre depuis url 
+$oeuvre_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+//Trouver l'oeuvre correspondante a l'ID 
+$oeuvre = null;
+foreach ($oeuvres as $item) {
+    if ($item['id'] == $oeuvre_id) {
+        $oeuvre = $item;
+        break;
+    }
+}
+// Verifier si l'oeuvre a ete trouver 
+
+if (!$oeuvre) {
+    header("Location: index.php"); // Redirection en cas d'oeuvre inexistante 
+    exit;
+}
+
+?>
 <main>
-    <?php 
-     // inclure le fichier des oeuvres pour recuperer les données 
+    <article id="detail-oeuvre">
+        <div id="img-oeuvre">
+            <img src="<?php echo '/the-artbox/' . htmlspecialchars($oeuvre['image']); ?>" alt="<?php echo htmlspecialchars($oeuvre['titre']); ?>">
+        </div>
+        <div id="contenu-oeuvre">
+        <h1><?php echo htmlspecialchars($oeuvre['titre']); ?></h1>
+            <p class="description"><?php echo htmlspecialchars($oeuvre['artiste']); ?></p>
+            <p class="description-complete">
+                <?php echo nl2br(htmlspecialchars($oeuvre['description'])); ?>
+            </p>
+        </div>
+    </article>
+</main>
 
-     include 'oeuvres.php';
-
-     $oeuvre_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-
-     echo $oeuvre_id 
-
-     ?>
- </main>
- <?php include('footer.php'); ?>
+<?php require_once 'footer.php';?>
